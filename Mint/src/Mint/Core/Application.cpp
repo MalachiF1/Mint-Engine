459a2331260ecb5b04a055ce11c3dd7767ce6070
Malachi <malachi.fraenkel@gmail.com>
2023-02-26 19:25:14 +0200

    Added buffer layouts.

--- Mint/src/Mint/Core/Application.cpp
+++ Mint/src/Mint/Core/Application.cpp
@@ -12,6 +12,28 @@ namespace mint
 
     Application* Application::s_instance = nullptr;
 
+    // Temporary
+    static GLenum shaderDataTypeToOpenGLDataType(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case ShaderDataType::Float: return GL_FLOAT;
+            case ShaderDataType::Float2: return GL_FLOAT;
+            case ShaderDataType::Float3: return GL_FLOAT;
+            case ShaderDataType::Float4: return GL_FLOAT;
+            case ShaderDataType::Int: return GL_INT;
+            case ShaderDataType::Int2: return GL_INT;
+            case ShaderDataType::Int3: return GL_INT;
+            case ShaderDataType::Int4: return GL_INT;
+            case ShaderDataType::Mat3: return GL_FLOAT;
+            case ShaderDataType::Mat4: return GL_FLOAT;
+            case ShaderDataType::Bool: return GL_BOOL;
+        }
+
+        MINT_CORE_ASSERT(false, "Unknown ShaderDataType!");
+        return 0;
+    }
+
     Application::Application()
     {
         MINT_CORE_ASSERT(!s_instance, "There can only be one Application instance!");
@@ -27,11 +49,36 @@ namespace mint
         glGenVertexArrays(1, &m_vertexArrray);
         glBindVertexArray(m_vertexArrray);
 
-        float vertices[] = { -0.5f, -0.5f, 0.0f, 0.5, -0.5f, 0.0f, 0.0f, 0.5f, 0.0f };
+        float vertices[] = {
+            // Positions        // Colors
+            -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // vertex 1
+            0.5,   -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, // vertex 2
+            0.0f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 1.0f  // vertex3
+        };
+
         m_vertexBuffer.reset(VertexBuffer::create(vertices, sizeof(vertices)));
 
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+        {
+            BufferLayout layout = { { ShaderDataType::Float3, "a_Pos" }, { ShaderDataType::Float4, "a_Color" } };
+
+            m_vertexBuffer->setLayout(layout);
+        }
+
+        uint32_t index     = 0;
+        const auto& layout = m_vertexBuffer->getLayout();
+        for (const auto& element : layout.getElements()) // layout.elements
+        {
+            glEnableVertexAttribArray(index);
+            glVertexAttribPointer(
+                index,
+                element.getComponentCount(),
+                shaderDataTypeToOpenGLDataType(element.type),
+                element.normalized ? GL_TRUE : GL_FALSE,
+                layout.getStride(),
+                (const void*)element.offset
+            );
+            ++index;
+        }
 
         uint32_t indices[] = { 0, 1, 2 };
         m_indexBuffer.reset(IndexBuffer::create(indices, std::size(indices)));
@@ -41,12 +88,15 @@ namespace mint
             #version 330 core
 
             layout(location = 0) in vec3 a_Pos;
+            layout(location = 1) in vec4 a_Color;
 
             out vec3 v_Pos;
+            out vec4 v_Color;
             
             void main()
             {
                 v_Pos = a_Pos;
+                v_Color = a_Color;
                 gl_Position = vec4(a_Pos, 1);
             }
         )";
@@ -57,10 +107,11 @@ namespace mint
             layout(location = 0) out vec4 color;
 
             in vec3 v_Pos;
+            in vec4 v_Color;
             
             void main()
             {
-                color = vec4(v_Pos * 0.5 + 0.5, 1.0);
+                color = v_Color;
             }
         )";
 
@@ -118,10 +169,10 @@ namespace mint
             glBindVertexArray(m_vertexArrray);
             glDrawElements(GL_TRIANGLES, m_indexBuffer->getCount(), GL_UNSIGNED_INT, nullptr);
 
-            for (Layer* layer : m_layerStack.m_layers) { layer->onUpdate(); }
+            for (auto layer : m_layerStack.getLayers()) { layer->onUpdate(); }
 
             m_ImGuiLayer->begin();
-            for (Layer* layer : m_layerStack.m_layers) { layer->onImGuiRender(); }
+            for (auto layer : m_layerStack.getLayers()) { layer->onImGuiRender(); }
             m_ImGuiLayer->end();
 
             m_window->onUpdate();
--- Mint/src/Mint/Renderer/Buffer.h
+++ Mint/src/Mint/Renderer/Buffer.h
@@ -3,6 +3,118 @@
 namespace mint
 {
 
+    // Buffer Layout
+    // ----------------------------------------------------------------------------
+
+    enum class ShaderDataType
+    {
+        None = 0,
+        Float,
+        Float2,
+        Float3,
+        Float4,
+        Int,
+        Int2,
+        Int3,
+        Int4,
+        Mat3,
+        Mat4,
+        Bool
+    };
+
+    static size_t shaderDataTypeSize(ShaderDataType type)
+    {
+        switch (type)
+        {
+            case ShaderDataType::Float: return 4;
+            case ShaderDataType::Float2: return 4 * 2;
+            case ShaderDataType::Float3: return 4 * 3;
+            case ShaderDataType::Float4: return 4 * 4;
+            case ShaderDataType::Int: return 4;
+            case ShaderDataType::Int2: return 4 * 2;
+            case ShaderDataType::Int3: return 4 * 3;
+            case ShaderDataType::Int4: return 4 * 4;
+            case ShaderDataType::Mat3: return 4 * 3 * 3;
+            case ShaderDataType::Mat4: return 4 * 4 * 4;
+            case ShaderDataType::Bool: return 1;
+        }
+
+        MINT_CORE_ASSERT(false, "Unknown ShaderDataType!");
+        return 0;
+    }
+
+    struct BufferElement
+    {
+        std::string name;
+        ShaderDataType type;
+        bool normalized;
+        uint32_t offset;
+        uint32_t size;
+
+        BufferElement() = default;
+
+        BufferElement(ShaderDataType type, const std::string& name, bool normalized = false) :
+            name(name), type(type), normalized(normalized), size(shaderDataTypeSize(type)), offset(0)
+        {
+        }
+
+        uint32_t getComponentCount() const
+        {
+            switch (type)
+            {
+                case ShaderDataType::Float: return 1;
+                case ShaderDataType::Float2: return 2;
+                case ShaderDataType::Float3: return 3;
+                case ShaderDataType::Float4: return 4;
+                case ShaderDataType::Int: return 1;
+                case ShaderDataType::Int2: return 2;
+                case ShaderDataType::Int3: return 3;
+                case ShaderDataType::Int4: return 4;
+                case ShaderDataType::Mat3: return 3 * 3;
+                case ShaderDataType::Mat4: return 4 * 4;
+                case ShaderDataType::Bool: return 1;
+            }
+
+            MINT_CORE_ASSERT(false, "Unknown ShaderDataType!");
+            return 0;
+        }
+    };
+
+    class BufferLayout
+    {
+        using Elements = std::vector<BufferElement>;
+
+      public:
+        BufferLayout() = default;
+
+        BufferLayout(const std::initializer_list<BufferElement>& elements) : m_elements(elements)
+        {
+            calculateOffsetAndStride();
+        }
+
+        inline const std::vector<BufferElement>& getElements() const { return m_elements; }
+        inline uint32_t getStride() const { return m_stride; }
+
+        Elements::const_iterator begin() const { return m_elements.begin(); }
+        Elements::const_iterator end() const { return m_elements.begin(); }
+
+      private:
+        void calculateOffsetAndStride()
+        {
+            m_stride = 0;
+            for (auto& element : m_elements)
+            {
+                element.offset = m_stride;
+                m_stride += element.size;
+            }
+        }
+
+      private:
+        Elements m_elements;
+        uint32_t m_stride = 0;
+    };
+
+
     // Vertex Buffer
     // ----------------------------------------------------------------------------
 
@@ -14,11 +126,16 @@ namespace mint
         virtual void bind() const   = 0;
         virtual void unbind() const = 0;
 
+        virtual const BufferLayout& getLayout() const      = 0;
+        virtual void setLayout(const BufferLayout& layout) = 0;
+
         static VertexBuffer* create(float* vertices, size_t size);
     };
 
+
     // Index Buffer
     // ----------------------------------------------------------------------------
+
     class IndexBuffer
     {
       public:
